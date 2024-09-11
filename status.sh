4a7250d0d791dad0f04a725c78b598c8ed883fe4
Nekorobi <177852383+nekorobi@users.noreply.github.com>
2024-09-11 18:44:33 +0900

    test: set timeout

--- status.sh
+++ status.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+set -e
 echo '#' warp-cli --accept-tos status
 warp-cli --accept-tos status
+sleep 3 # wait
 
-args="--fail --silent https://www.cloudflare.com/cdn-cgi/trace/"
+url=https://www.cloudflare.com/cdn-cgi/trace/
+args="--connect-timeout 3 --max-time 3 --retry 3 --fail --no-progress-meter $url"
 echo '#' curl -4 $args
-curl4=$(curl -4 $args) || exit 4
+curl4=$(curl -4 $args)
 echo "$curl4"
 
 echo '#' curl -6 $args
-curl6=$(curl -6 $args) || exit 6
+curl6=$(curl -6 $args)
 echo "$curl6"
 
 echo "$curl4" | grep '^warp=on$' >/dev/null || exit 4
